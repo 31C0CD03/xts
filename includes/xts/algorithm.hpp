c6fd80601935eebb4f10bd8e8599cdeafaed4f57
31C0CD03 <31C0CD03@proton.me>
2026-06-16 15:55:58 +0100

    feat: add avx512 accelerated pattern scan

--- includes/xts/algorithm.hpp
+++ includes/xts/algorithm.hpp
@@ -9,7 +9,7 @@
 
 #if defined( __ARM_NEON ) || defined( __ARM_NEON__ )
 	#include <arm_neon.h>
-#elif defined( __AVX2__ )
+#elif defined( __AVX2__ ) || defined( __AVX512F__ )
 	#include <immintrin.h>
 #endif
 
@@ -79,6 +79,63 @@ namespace xts
 				// If all bytes are 0 then continue matching
 				//
 				match &= ( vgetq_lane_u64( diff_vec, 0 ) | vgetq_lane_u64( diff_vec, 1 ) ) == 0;
+				if ( !match )
+					break;
+			}
+
+			// Compare straggler bytes individually if in the middle of a match
+			//
+			for ( ; match && ( j < pattern_len ); j++ )
+				if ( !( match &= ( ( data[ i + j ] ^ pattern[ j ] ) & mask[ j ] ) == 0 ) )
+					break;
+
+			if ( match )
+				return i;
+		}
+
+		return -1;
+	}
+
+#elif defined( __AVX512F__ )
+
+	// AVX512 accelerated pattern scanner
+	//
+	static std::ptrdiff_t scan_impl( const std::uint8_t* data, const std::size_t data_len, const std::uint8_t* pattern, const std::size_t pattern_len, const std::uint8_t* mask )
+	{
+		// Data must not be shorter than the pattern
+		//
+		assert( data_len >= pattern_len );
+
+		// Detect alignment for pattern and mask,
+		// Data indexed by incrementing offset thus not eligible
+		//
+		const bool pattern_aligned = ( reinterpret_cast<std::uintptr_t>( pattern ) & 63 ) == 0;
+		const bool mask_aligned    = ( reinterpret_cast<std::uintptr_t>( mask ) & 63 ) == 0;
+
+		// Valid slices are (0 .. (pattern_len - 1)) through ((data_len - pattern_len) .. (data_len - 1))
+		//
+		for ( std::size_t i = 0; i <= ( data_len - pattern_len ); i++ )
+		{
+			int match = 1;
+
+			// Compare 64 bytes of the pattern at a time
+			//
+			std::size_t j = 0;
+			for ( ; j < ( pattern_len & ~63 ); j += 64 )
+			{
+				// Detect alignment
+				//
+				__m512i data_vec    = _mm512_loadu_si512( reinterpret_cast<const __m512i*>( &data[ i + j ] ) );
+				__m512i pattern_vec = pattern_aligned ? _mm512_load_si512( reinterpret_cast<const __m512i*>( &pattern[ j ] ) ) : _mm512_loadu_si512( reinterpret_cast<const __m512i*>( &pattern[ j ] ) );
+				__m512i mask_vec    = mask_aligned ? _mm512_load_si512( reinterpret_cast<const __m512i*>( &mask[ j ] ) ) : _mm512_loadu_si512( reinterpret_cast<const __m512i*>( &mask[ j ] ) );
+
+				// Compute the diff, mask wildcard bytes to 0
+				//
+				const __m512i diff_vec = _mm512_and_si512( _mm512_xor_si512( data_vec, pattern_vec ), mask_vec );
+
+				// If all bytes are 0 then continue matching
+				//
+				match &= _mm512_test_epi64_mask( diff_vec, _mm512_setzero_si512() ) == 0;
 				if ( !match )
 					break;
 			}
